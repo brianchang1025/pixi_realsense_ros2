bac91d267a12194c1a94135a60c3693ab447dfc6
brianchang1025 <brianchang4591@gmail.com>
2026-02-20 15:20:18 +0100

    remove redundancy file

--- scripts/run_rqt.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-RQT_CMD="pixi run -e jazzy rqt --perspective-file ./scripts/cameraviewer.perspective"
-
-
-echo "Launching rqt: $RQT_CMD"
-# Start rqt
-eval "$RQT_CMD"
